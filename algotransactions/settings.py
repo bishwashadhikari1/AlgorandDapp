db0a0e63f4794272bc5041e7b30a4485d09323bf
bishwashadhikari1 <65892817+bishwashadhikari1@users.noreply.github.com>
2022-09-04 15:44:37 +0545

    Update settings.py

--- algotransactions/settings.py
+++ algotransactions/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)$m&0gl=($)5&1g_2oritu(=0v-+maz)9#h(2hd&j$r^-o(3n'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
